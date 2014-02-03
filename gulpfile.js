d6c955730e2c6e19035511fba52790ff2d9e2f12
Brad Dunn <brad@braddunn.com>
2014-02-03 14:12:50 -0500

    Added in more gulp task stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
+// Gulp and Plugins
 var gulp          = require('gulp'),
     gutil         = require('gulp-util'),
+    jshint        = require('gulp-jshint'),
     less          = require('gulp-less'),
     autoprefixer  = require('gulp-autoprefixer'),
     minifycss     = require('gulp-minify-css'),
@@ -14,8 +16,23 @@ var gulp          = require('gulp'),
     lr            = require('tiny-lr'),
     server        = lr();
 
+// Styles
 gulp.task('styles', function(){
   return gulp.src('app/styles/main.less')
+    .pipe(less())
+    .pipe(minifycss())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// Scripts
+gulp.tasl('scripts', function(){
+  return gulp.src('app/scripts/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(contact('main.js'))
+    .pipe(gulp.dest('dist/assets/js'))
 });
 
 gulp.task('default', function(){
