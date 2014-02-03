9f8d665b1d9eb325a40efdd8f6c519555d29734b
Brad Dunn <brad@braddunn.com>
2014-02-03 11:48:38 -0500

    Added in gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+var gulp          = require('gulp'),
+    gutil         = require('gulp-util'),
+    less          = require('gulp-less'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    minifycss     = require('gulp-minify-css'),
+    uglify        = require('gulp-uglify'),
+    imagemin      = require('gulp-imagemin'),
+    rename        = require('gulp-rename'),
+    clean         = require('gulp-clean'),
+    concat        = require('gulp-concat'),
+    notify        = require('gulp-notify'),
+    cache         = require('gulp-cache'),
+    livereload    = require('gulp-livereload'),
+    lr            = require('tiny-lr'),
+    server        = lr();
 
+gulp.task('styles', function(){
+  return gulp.src('app/styles/main.less')
+});
 
 gulp.task('default', function(){
   // The Default Task
